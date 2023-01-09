9de4aaebc908b3e1321b09a67ea5d8b3a8db3f01
hosseingholizadeh <hossein.hg901@gmail.com>
2023-01-09 11:17:01 +0330

    changes in the stylig and the preview

--- src/Components/Element/Elements/RenderDropDown.js
+++ src/Components/Element/Elements/RenderDropDown.js
@@ -1,33 +1,42 @@
 import { Select } from "antd";
+import { RenderType } from "../ElementType";
 import { renderStyle } from "../RenderUtils";
 
-export default function RenderDropDown({ t, element }) {
-  let { options, name, left, top } = element;
+export default function RenderDropDown({ t, element, renderType }) {
+  let { options, name } = element;
+  console.log(element);
   let { style } = options ?? {};
+  let isReadonly = renderType === RenderType.dragdrop;
+
+  console.log("drop down is readonly ", isReadonly);
 
   return (
     <Select
-      defaultValue="lucy"
+      aria-readonly={isReadonly}
       style={renderStyle(style)}
-      options={[
-        {
-          value: "jack",
-          label: "Jack",
-        },
-        {
-          value: "lucy",
-          label: "Lucy",
-        },
-        {
-          value: "disabled",
-          disabled: true,
-          label: "Disabled",
-        },
-        {
-          value: "Yiminghe",
-          label: "yiminghe",
-        },
-      ]}
+      options={
+        isReadonly
+          ? []
+          : [
+              {
+                value: "jack",
+                label: "Jack",
+              },
+              {
+                value: "lucy",
+                label: "Lucy",
+              },
+              {
+                value: "disabled",
+                disabled: true,
+                label: "Disabled",
+              },
+              {
+                value: "Yiminghe",
+                label: "yiminghe",
+              },
+            ]
+      }
     />
   );
 }
