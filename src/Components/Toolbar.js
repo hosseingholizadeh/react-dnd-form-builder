09402e28f2fa07d665d45aeada482a2992dd4867
hosseingholizadeh <hossein.hg901@gmail.com>
2023-01-12 11:17:31 +0330

    some styles are fixed with form setting firs steps are added

--- src/Components/Toolbar.js
+++ src/Components/Toolbar.js
@@ -1,11 +1,25 @@
 import { Button, Space } from "antd";
+import { useState } from "react";
+import FormSetting from "./FormSetting";
 
-export default function Toolbar({ t, openPreview }) {
+export default function Toolbar({ t, openPreview, form, setFormData }) {
+  const [formSettingVisible, setFormSettingVisible] = useState(false);
+
   return (
     <div class="row fb-toolbar">
+      <FormSetting
+        visible={formSettingVisible}
+        close={() => setFormSettingVisible(false)}
+        t={t}
+        form={form}
+        setFormData={setFormData}
+      />
       <div className="col-12">
         <Space>
-          <Button type="primary" onClick={openPreview}>
+          <Button type="primary" onClick={() => setFormSettingVisible(true)}>
+            {t("formSetting")}
+          </Button>
+          <Button type="dashed" danger onClick={openPreview}>
             {t("preview")}
           </Button>
         </Space>
