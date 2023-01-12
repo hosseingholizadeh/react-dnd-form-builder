693ca76dd0aa2b801a11de9b82373d98b0d21cd4
hosseingholizadeh <hossein.hg901@gmail.com>
2023-01-12 09:55:35 +0330

    checbox group to single one

--- src/Components/Element/Elements/RenderCheckbox.js
+++ src/Components/Element/Elements/RenderCheckbox.js
@@ -2,8 +2,22 @@ import { Checkbox } from "antd";
 import { renderStyle } from "../RenderUtils";
 
 export default function PreviewCheckbox({ t, element }) {
+  let { options, name, defaultValue } = element;
+  let { style, title } = options ?? {};
+
+  if (!title || title.trim() === "") title = name;
+
+  return (
+    <Checkbox style={renderStyle(style)} checked={defaultValue}>
+      {title}
+    </Checkbox>
+  );
+}
+
+function PreviewCheckboxGroup({ t, element }) {
   let { options, name } = element;
   let { style } = options ?? {};
+
   const plainOptions = ["Apple", "Pear", "Orange"];
   const defaultCheckedList = ["Apple", "Orange"];
 
